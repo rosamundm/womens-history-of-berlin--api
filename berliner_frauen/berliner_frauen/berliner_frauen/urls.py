8b40a1ba51c4b6159b2a5dd05ee70a90271eb1b4
Rosamund Mather <rsmnd.mthr@gmail.com>
2021-12-04 18:19:32 +0100

    Set up SimpleJWT

--- berliner_frauen/berliner_frauen/berliner_frauen/urls.py
+++ berliner_frauen/berliner_frauen/berliner_frauen/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_jwt.views import (
-    obtain_jwt_token,
-    refresh_jwt_token,
-    verify_jwt_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
+
 from streets.views import DistrictViewSet
 
 
@@ -31,16 +32,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("streets.urls")),
-
     path("api/v1/", include(router.urls)),
-    path("api/v1/token-auth/", obtain_jwt_token),
-    path("api/v1/token-refresh/", refresh_jwt_token),
-    path("api/v1/token-verify/", verify_jwt_token),
-
+    path("api/v1/token-auth/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token-refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/v1/token-verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("districts/", DistrictViewSet.as_view({"get": "list"}), name="districts"),
 ]
-
-"""
-list: /api/v1/districts/
-detail: /api/v1/districts/1/
-
-"""
--- berliner_frauen/berliner_frauen/streets/views.py
+++ berliner_frauen/berliner_frauen/streets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import District
 from .serializers import DistrictSerializer
 
@@ -10,4 +11,5 @@ def index(request):
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.all().order_by("name")
     serializer_class = DistrictSerializer
+    authentication_class = [JWTAuthentication]
     # permission_classes = [permissions.IsAuthenticated]
