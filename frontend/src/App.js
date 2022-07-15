2654e2dd122bce614e70d74192fd63b1263e61b5
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-07-15 22:02:25 +0200

    Refactor App component to function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutPage from "./components/About.js";
+import DistrictBrowser from "./components/DistrictBrowser.js";
 import DistrictInstance from "./components/District.js";
-// import DistrictBrowser from "./components/Districts.js";
 import StreetInstance from "./components/Street.js";
 import Footer from "./components/layout/Footer.js";
 import Header from "./components/layout/Header.js";
+import HomepageHeader from "./components/layout/HomepageHeader.js";
 import HomePage from "./components/Home.js";
 
-class App extends Component {
-
-    render() {
+export default function App() {
 
       return (
 
           <main className="text-indigo-800 bg-teal-100 body-font min-h-screen">
-
-            <Header/>  
 
             <Router>
               <Routes>
@@ -24,7 +21,7 @@ class App extends Component {
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/about" element={<AboutPage/>}/>
 
-                {/*<Route path="/districts" element={<DistrictBrowser/>}/>*/}
+                <Route path="/districts" element={<DistrictBrowser/>}/>
                 <Route path="/districts/:district_slug" element={<DistrictInstance/>}/>
                 <Route path="/districts/:district_slug/:street_slug" element={<StreetInstance/>}/>
 
@@ -34,12 +31,5 @@ class App extends Component {
             <Footer/>
 
           </main>
-
       )
-    }
 };
-
-export default App;
-
-
-
