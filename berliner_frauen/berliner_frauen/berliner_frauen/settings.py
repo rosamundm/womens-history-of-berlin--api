d57c81c1e2747692c6b8df40ffc2248efeb14f54
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-08-28 21:16:12 +0200

    Add new database variables

--- berliner_frauen/berliner_frauen/berliner_frauen/settings.py
+++ berliner_frauen/berliner_frauen/berliner_frauen/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = "berliner_frauen.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT")
     }
 }
+
 
 
 # Password validation
