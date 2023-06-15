8beb223d1cedc095820f254207debfd5ab28ba85
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-06-15 16:49:36 +0200

    Correct settings

--- berliner_frauen/berliner_frauen/berliner_frauen/settings.py
+++ berliner_frauen/berliner_frauen/berliner_frauen/settings.py
@@ -18,10 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
