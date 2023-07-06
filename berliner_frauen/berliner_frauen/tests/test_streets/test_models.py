3b5bab8d2d802f46317d236e3eb573f5ecbaeadf
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-07-06 19:42:48 +0200

    Add test__street_slug_generated

--- berliner_frauen/berliner_frauen/tests/test_streets/test_models.py
+++ berliner_frauen/berliner_frauen/tests/test_streets/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from tests.factories.streets import DistrictFactory
+from tests.factories.streets import DistrictFactory, StreetFactory
 
 
 @pytest.mark.django_db
@@ -8,3 +8,20 @@ def test__district_slug_generated():
     district = DistrictFactory.create(name="Chärlöttenbürg-Wilmerßdörf")
 
     assert district.district_slug == "chaerloettenbuerg-wilmerssdoerf"
+
+
+@pytest.mark.django_db
+def test__street_slug_generated():
+    
+    street = StreetFactory.create(
+        district=DistrictFactory.create(name="Mitte"),
+        name="Rösä-Lüxembürg-Sträße",
+        eponym_name="Rosa Luxemburg",
+        eponym_core_data_added=False,
+        entry_complete=False,
+        image_available=False,
+        tags=["politics", ],
+        map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    assert street.street_slug == "roesae-luexembuerg-straesse"
