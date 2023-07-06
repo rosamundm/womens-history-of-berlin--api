0b811e98c2cbc451999220ab2179ad219344e77c
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-07-06 20:42:55 +0200

    Add more streets.test_models

--- berliner_frauen/berliner_frauen/tests/test_streets/test_models.py
+++ berliner_frauen/berliner_frauen/tests/test_streets/test_models.py
@@ -25,3 +25,88 @@ def test__street_slug_generated():
     )
 
     assert street.street_slug == "roesae-luexembuerg-straesse"
+
+
+@pytest.mark.django_db
+def test__district_number_of_added_streets():
+
+    district = DistrictFactory.create(name="Mitte")
+
+    StreetFactory.create_batch(
+       3,
+       district=district,
+       name="Rösä-Lüxembürg-Sträße",
+       eponym_name="Rosa Luxemburg",
+       eponym_core_data_added=False,
+       entry_complete=False,
+       image_available=False,
+       tags=["politics", ],
+       map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    assert district.number_of_added_streets == 3
+
+
+@pytest.mark.django_db
+def test__district_number_of_completed_streets():
+
+    district = DistrictFactory.create(name="Mitte")
+
+    StreetFactory.create_batch(
+       2,
+       district=district,
+       name="Rösä-Lüxembürg-Sträße",
+       eponym_name="Rosa Luxemburg",
+       eponym_core_data_added=False,
+       entry_complete=True,
+       image_available=False,
+       tags=["politics", ],
+       map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    StreetFactory.create_batch(
+       2,
+       district=district,
+       name="Rösä-Lüxembürg-Sträße",
+       eponym_name="Rosa Luxemburg",
+       eponym_core_data_added=False,
+       entry_complete=False,
+       image_available=False,
+       tags=["politics", ],
+       map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    assert district.number_of_completed_streets == 2
+
+
+@pytest.mark.django_db
+def test__district_number_of_photos_taken():
+
+    district = DistrictFactory.create(name="Mitte")
+
+    StreetFactory.create_batch(
+       10,
+       district=district,
+       name="Rösä-Lüxembürg-Sträße",
+       eponym_name="Rosa Luxemburg",
+       eponym_core_data_added=False,
+       entry_complete=True,
+       image_available=True,
+       tags=["politics", ],
+       map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    StreetFactory.create_batch(
+       1,
+       district=district,
+       name="Rösä-Lüxembürg-Sträße",
+       eponym_name="Rosa Luxemburg",
+       eponym_core_data_added=False,
+       entry_complete=True,
+       image_available=False,
+       tags=["politics", ],
+       map_link="https://www.openstreetmap.org/way/109819106",
+    )
+
+    assert district.number_of_photos_taken == 10
+
