3e433a6e7c852a75c4d3ebdd0391d335ef5abdb2
Rosamund Mather <rsmnd.mthr@gmail.com>
2023-07-16 12:48:34 +0200

    Add street serializer tests

--- berliner_frauen/berliner_frauen/tests/factories/streets.py
+++ berliner_frauen/berliner_frauen/tests/factories/streets.py
@@ -1,3 +1,4 @@
+import datetime
 import factory
 from streets.models import District, Street
 
@@ -7,11 +8,24 @@ class DistrictFactory(factory.django.DjangoModelFactory):
         model = District
 
     name = factory.Sequence(lambda n: "Chärlöttenbürg-Wilmersdörf %d" % n)
-    image_path = factory.Sequence(
-        lambda n: "Schloss_Clbg.jpeg %d" % n
-    )
+    image_path = "Schloss_Clbg.jpeg"
 
 
 class StreetFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Street
+
+    name = factory.Sequence(lambda n: "Rösä-Lüxembürg-Sträße %d" % n)
+    district = factory.SubFactory(DistrictFactory)
+    map_link = "https://www.openstreetmap.org/way/109819106"
+    eponym_name = "Rosa Luxemburg"
+    eponym_date_of_birth = datetime.date(1871, 3, 5)
+    eponym_date_of_death = datetime.date(1919, 1, 15)
+    eponym_place_of_birth = "Zamość, Poland"
+    eponym_place_of_death = "Berlin, Germany"
+    eponym_description = "<p>Ipsum suspendisse ultrices gravida</p>"
+    image = "luxemburg_berlin.jpeg"
+    image_available = True
+    eponym_core_data_added = True
+    entry_complete = True
+    tags = ["politics", ],
--- berliner_frauen/berliner_frauen/tests/test_streets/test_serializers.py
+++ berliner_frauen/berliner_frauen/tests/test_streets/test_serializers.py
@@ -25,10 +25,11 @@ def test__get_district_list(api_request, api_url, api_user):
     assert response.status_code == 200
 
     for i in range(0, 3):
-        assert "Chärlöttenbürg-Wilmersdörf" == data[i]["name"]
-        assert "Schloss_Clbg.jpeg" == data[i]["image_path"]
+        assert lambda n: "Chärlöttenbürg-Wilmersdörf %d" % n \
+              == data[i]["name"]
         assert lambda n: "chaerloettenbuerg-wilmerssdoerf %d" % n == \
             data[i]["district_slug"]
+        assert "Schloss_Clbg.jpeg" == data[i]["image_path"]
 
 
 @pytest.mark.django_db
@@ -49,8 +50,60 @@ def test__get_district_instance(api_request, api_url, api_user):
     data = json.loads(data)
 
     assert response.status_code == 200
-    assert "Chärlöttenbürg-Wilmersdörf" == data["name"]
-    assert "Schloss_Clbg.jpeg" == data["image_path"]
+    assert lambda n: "Chärlöttenbürg-Wilmersdörf %d" % n == data["name"]
     assert lambda n: "chaerloettenbuerg-wilmerssdoerf %d" % n == \
         data["district_slug"]
+    assert "Schloss_Clbg.jpeg" == data["image_path"]
+    
 
+@pytest.mark.django_db
+def test__get_street_list(api_request, api_url, api_user):
+
+    StreetFactory.create_batch(3)
+
+    view = StreetViewSet.as_view(actions={"get": "list"})
+
+    request = api_request.get(
+        f"{api_url}streets/",
+        content_type="application/json"
+    )
+    
+    force_authenticate(request, user=api_user)
+    response = view(request)
+    response = response.render()
+    data = json.dumps(response.data)
+    data = json.loads(data)
+
+    assert response.status_code == 200
+    for i in range(0, 3):
+        assert lambda n: "Rösä-Lüxembürg-Sträße %d" % n \
+              == data[i]["name"]
+        assert lambda n: "roesae-luexembuerg-straesse %d" % n == \
+            data[i]["street_slug"]
+        assert "05.03.1871" == data[i]["eponym_date_of_birth"]
+        assert "15.01.1919" == data[i]["eponym_date_of_death"]
+
+
+@pytest.mark.django_db
+def test__get_street_instance(api_request, api_url, api_user):
+
+    street = StreetFactory.create()
+
+    view = StreetViewSet.as_view(actions={"get": "retrieve"})
+
+    request = api_request.get(
+        f"{api_url}streets/",
+        content_type="application/json"
+    )
+    
+    force_authenticate(request, user=api_user)
+    response = view(request, street_slug=street.street_slug)
+    response = response.render()
+    data = json.dumps(response.data)
+    data = json.loads(data)
+
+    assert response.status_code == 200
+    assert lambda n: "Rösä-Lüxembürg-Sträße %d" % n == data["name"]
+    assert lambda n: "roesae-luexembuerg-straesse %d" % n == data["street_slug"]
+    assert "05.03.1871" == data["eponym_date_of_birth"]
+    assert "15.01.1919" == data["eponym_date_of_death"]
