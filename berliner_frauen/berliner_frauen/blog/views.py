58e332696a32c69521f0b9b470a3f72cbf56245d
Rosamund Mather <rsmnd.mthr@gmail.com>
2024-08-25 16:15:57 +0200

    Change queryset

--- berliner_frauen/berliner_frauen/blog/views.py
+++ berliner_frauen/berliner_frauen/blog/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.utils import timezone
 from rest_framework import permissions, viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import BlogPost
@@ -5,9 +7,9 @@ from .serializers import BlogPostSerializer
 
 
 class BlogPostViewSet(viewsets.ModelViewSet):
-    queryset = (
-        BlogPost.objects.filter(ready_to_publish=True).order_by("-published")
-    )
+    queryset = BlogPost.objects.filter(
+        Q(ready_to_publish=True) & Q(published__lte=timezone.now())
+    ).order_by("-published")
     serializer_class = BlogPostSerializer
     lookup_field = "slug"
     authentication_class = JWTAuthentication
